332c847beedb82fb5ed24c76bfcaea804a66f445
rungene <rungenesamuel@gmail.com>
2018-06-05 20:57:14 +0300

    updating the RemiderTask class

--- app/src/main/java/com/example/android/background/sync/ReminderTasks.java
+++ app/src/main/java/com/example/android/background/sync/ReminderTasks.java
@@ -4,13 +4,21 @@ package com.example.android.background.sync;
 
 import android.content.Context;
 
+import com.example.android.background.utilities.PreferenceUtilities;
+
 public class ReminderTasks{
     public static  String ACTION_INCREMENT_WATER_COUNT;
+    private static void incrementWaterCount(Context context){
+
+        PreferenceUtilities.incrementWaterCount(context);
+    }
 
 
     public static void executeTask(Context context,String action){
         if (action==ACTION_INCREMENT_WATER_COUNT){
-            
+            incrementWaterCount(context);
+
+
         }
 
     }
---
+++ app/src/main/java/com/example/android/background/utilities/ReminderTasks.java
@@ -0,0 +1,7 @@
+package com.example.android.background.utilities;
+
+public class ReminderTasks {
+
+
+
+}
