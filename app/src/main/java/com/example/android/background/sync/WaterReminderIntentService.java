ba568a25276b98c74c5c5acf1acfc3e6f753ca7e
rungene <rungenesamuel@gmail.com>
2018-06-06 18:57:42 +0300

    updating the WaterReminderIntentService class and manifest file

--- app/src/main/java/com/example/android/background/sync/WaterReminderIntentService.java
+++ app/src/main/java/com/example/android/background/sync/WaterReminderIntentService.java
@@ -1,4 +1,4 @@
-/*
+package com.example.android.background.sync;/*
  * Copyright (C) 2016 The Android Open Source Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
@@ -11,8 +11,32 @@
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
+ *
  * limitations under the License.
  */
+
+import android.app.IntentService;
+import android.content.Intent;
+import android.support.annotation.Nullable;
+
+public class WaterReminderIntentService extends IntentService{
+
+
+    public WaterReminderIntentService(String name) {
+        super(name);
+    }
+
+    @Override
+    protected void onHandleIntent(@Nullable Intent intent) {
+
+        String action = intent.getAction();
+        ReminderTasks.executeTask(this,action);
+
+
+
+    }
+
+}
 
 // TODO (9) Create WaterReminderIntentService and extend it from IntentService
 
